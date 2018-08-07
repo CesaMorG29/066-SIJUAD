35c4420c281611826b0cc39a5cd28b4771c7b27b
CesarCDS <CesarCDS@192.168.52.38>
2018-08-07 09:18:20 -0500

    Proyecto de Nueva Creación

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        TRIBUNAL DE JUISTICIA ADMINISTRATIVA
+        <?php
+        ?>
+    </body>
+</html>
